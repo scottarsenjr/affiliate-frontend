7ec889a87a010656b6799d4ecb0da4221d1fdcfe
scxrchy <scxrchybeats@gmail.com>
2024-06-26 22:09:39 +0300

    Dockerize React App & Fix Readme.md

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
